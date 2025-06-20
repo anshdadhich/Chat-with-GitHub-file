4b133029b04d1c9b62f46f20589e6edd8d52e72a
anshdadhich <anshdadhichmm@gmail.com>
2025-06-20 16:28:53 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,11 +1,13 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from google import genai
 
+YOUR_API_KEY = ""
+
 embeddings = GoogleGenerativeAIEmbeddings(
     model = "models/embedding-001",
-    google_api_key = "AIzaSyD0I7tw8w9wo3c7BFebS9PeJimi_GJWkT0"
+    google_api_key = YOUR_API_KEY
     )
 
-client = genai.Client(api_key = "AIzaSyD0I7tw8w9wo3c7BFebS9PeJimi_GJWkT0")
+client = genai.Client(api_key = YOUR_API_KEY)
 
 model = client.models
